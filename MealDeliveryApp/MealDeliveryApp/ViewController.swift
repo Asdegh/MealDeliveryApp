94080a68a67048963adcfdd855a840e7191c2a4c
Sergei Anisimov <serge.anisimoff@gmail.com>
2024-02-10 19:52:19 +0300

    Created base project, removed storyboard references.

---
+++ MealDeliveryApp/MealDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealDeliveryApp
+//
+//  Created by Alex Murphy on 10.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .purple
+	}
+
+
+}
+
